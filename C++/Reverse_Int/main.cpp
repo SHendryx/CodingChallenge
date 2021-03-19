e83a0d1e0a2c8c3a2a7ff26c7725398704fa70ed
Samuel Hendryx <samhendryx@gmail.com>
2021-03-18 19:44:15 -0700

    refine code slightly. update README doc.

--- C++/Reverse_Int/main.cpp
+++ C++/Reverse_Int/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
 int reverseInt(int x){
-    std::int32_t revInt = 0;
+    int revInt = 0;
+    int pop = 0;
     std::cout << "Initial int: " << x << "\n";
     while (x != 0){
+        pop = x % 10;
         // Test 32-bit integer overflow
         if ((revInt > (INT32_MAX / 10)) || (revInt < (INT32_MIN / 10))){
             return 0;
         }
-        revInt = (revInt * 10) + (x % 10);
+        revInt = (revInt * 10) + pop;
         x = (x / 10);
     }
     return revInt;
@@ -16,6 +18,6 @@ int reverseInt(int x){
 int main() {
     std::cout << "Max Int: " << INT32_MAX << "\n";
     std::cout << "Min Int: " << INT32_MIN << "\n";
-    std::cout << reverseInt(1463847412);
+    std::cout << reverseInt(123);
     return 0;
 }
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ A collection of coding challenges in various forms including some mathematical c
 
 ## ReverseInt
 Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+### Solution:
+Not 100% sure the solution will actually allow an integer to equal the INT32_MAX or INT32_MIN values.
