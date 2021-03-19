4e246a2bdd7c0fcd276739b861ab73b75678ba45
Samuel Hendryx <samhendryx@gmail.com>
2021-03-18 17:04:55 -0700

    test 32-bit int overflow

--- C++/Reverse_Int/main.cpp
+++ C++/Reverse_Int/main.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
-
-class reverseInt {
-private:
-    int revInt = 0;
-public:
     int reverse(int x){
+        std::int32_t revInt = 0;
         std::cout << "Initial int: " << x << "\n";
         while (x != 0){
-            //std::cout << "Value of x % 10: " << (x % 10) << "\n";
+            // Test 32-bit integer overflow
+            if ((revInt > (INT32_MAX / 10)) || (revInt < (INT32_MIN / 10))){
+                return 0;
+            }
             revInt = (revInt * 10) + (x % 10);
-            //std::cout << "Current revInt: " << revInt << "\n";
             x = (x / 10);
-            //std::cout << "Current x value: " << x << "\n";
-        }
-        return revInt;
     }
-};
+    return revInt;
+    }
 int main() {
-    reverseInt myInt;
-    std::cout << myInt.reverse(-123);
+    std::cout << "Max Int: " << INT32_MAX << "\n";
+    std::cout << "Min Int: " << INT32_MIN << "\n";
+    std::cout << reverse(1463847412);
     return 0;
 }
