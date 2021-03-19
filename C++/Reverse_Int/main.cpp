67a130afb61432b910d2f5381c3e3d31038e2f04
Samuel Hendryx <samhendryx@gmail.com>
2021-03-18 17:09:54 -0700

    README text update and code cleanup

--- C++/Reverse_Int/main.cpp
+++ C++/Reverse_Int/main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-    int reverse(int x){
-        std::int32_t revInt = 0;
-        std::cout << "Initial int: " << x << "\n";
-        while (x != 0){
-            // Test 32-bit integer overflow
-            if ((revInt > (INT32_MAX / 10)) || (revInt < (INT32_MIN / 10))){
-                return 0;
-            }
-            revInt = (revInt * 10) + (x % 10);
-            x = (x / 10);
+
+int reverseInt(int x){
+    std::int32_t revInt = 0;
+    std::cout << "Initial int: " << x << "\n";
+    while (x != 0){
+        // Test 32-bit integer overflow
+        if ((revInt > (INT32_MAX / 10)) || (revInt < (INT32_MIN / 10))){
+            return 0;
+        }
+        revInt = (revInt * 10) + (x % 10);
+        x = (x / 10);
     }
     return revInt;
-    }
+}
 int main() {
     std::cout << "Max Int: " << INT32_MAX << "\n";
     std::cout << "Min Int: " << INT32_MIN << "\n";
-    std::cout << reverse(1463847412);
+    std::cout << reverseInt(1463847412);
     return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CodingChallenge
 A collection of coding challenges in various forms including some mathematical challenges.
+
+## ReverseInt
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
